4a52f6bf7294d2b993ec954dc2225505dc423167
sathya <ssekar@gmail.com>
2023-02-07 15:29:33 +0530

    Change in book.js

--- router/books.js
+++ router/books.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const  Book  = require('../models/Book');
 
-router.get('/test',(req,res)=>{
+router.get('/tests',(req,res)=>{
   res.send('test2')
 })
 
